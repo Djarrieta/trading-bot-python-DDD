5f999c108c13a69edfd7171edeebf19a2d254498
Dario Arrieta <arrieta.dario@hotmail.com>
2023-05-10 12:08:15 -0500

    validate

---
+++ src/common/Validate.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass, fields
+
+
+@dataclass()
+class Validate:
+    def __post_init__(self):
+        for field in fields(self):
+            if getattr(self, field.name) is None:
+                raise ValueError(f"{field.name} must have a value.")
+
+    def validate_fields(self):
+        for field in fields(self):
+            if field.name not in self.__dict__:
+                raise AttributeError(
+                    f"{field.name} is missing from the {type(self).__name__} instance.")
+            if not getattr(self, field.name):
+                raise ValueError(
+                    f"{field.name} cannot be empty in the {type(self).__name__} instance.")
--- src/domain/UserData.py
+++ src/domain/UserData.py
@@ -1,8 +1,10 @@
-from dataclasses import dataclass, fields
+from dataclasses import dataclass
+
+from src.common.Validate import Validate
 
 
 @dataclass()
-class UserData:
+class UserData(Validate):
     user_id: str
     name: str
     start_date: str
@@ -10,17 +12,3 @@ class UserData:
     api_key: str
     api_secret: str
     max_risk_percent: int
-
-    def __post_init__(self):
-        for field in fields(self):
-            if getattr(self, field.name) is None:
-                raise ValueError(f"{field.name} must have a value.")
-
-    def validate_fields(self):
-        for field in fields(self):
-            if field.name not in self.__dict__:
-                raise AttributeError(
-                    f"{field.name} is missing from the {type(self).__name__} instance.")
-            if not getattr(self, field.name):
-                raise ValueError(
-                    f"{field.name} cannot be empty in the {type(self).__name__} instance.")
